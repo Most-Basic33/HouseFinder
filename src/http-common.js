4f2c867a19e3bf4f306919d99a3cbc51bb2b2393
bishopwalker <walkerbishop333@gmail.com>
2020-08-05 14:48:22 -0700

    m

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:6969/api",
+//  baseURL: "http://localhost:6969/api",
+baseURL:"http://64.227.104.65:6969/api",
   headers: {
     "Content-type": "application/json"
   },
