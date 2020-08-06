b5ff3155d2bf1d1ca8b35045590ce99b41a4a051
bishopwalker <walkerbishop333@gmail.com>
2020-08-06 13:00:23 -0700

    m

--- src/http-common.js
+++ src/http-common.js
@@ -3,9 +3,9 @@ import axios from "axios";
 console.log(process.env)
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_NODE_ENV === 'production' ? '/api' : 'http://localhost:6969/api',
+  baseURL: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:6969/api',
   headers: {
     "Content-type": "application/json"
   },
-withCredentials: true
+  withCredentials: true
 });
